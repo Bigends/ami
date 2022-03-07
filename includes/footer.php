4a54f6ff92dfae537922986219edf2792d32bce6
Bigends <chris.balkumar@outlook.fr>
2022-03-07 15:54:51 -0300

    index terminé

---
+++ includes/footer.php
@@ -0,0 +1,62 @@
+                <!-- newsletter area end -->
+
+                <!-- footer main -->
+                <div class="footer-main">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-6 col-lg-3 col-md-3 col-xs-12">
+                                <div class="footer-logo-area">
+                                    <a href="#"><img src="images/logo/amilogo2.png" alt="footer logo"></a>
+                                    <p>Alpha Monétique Intéractif. <br>La Monétique d'aujourd'hui.</p>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-lg-3 col-md-3 col-xs-12">
+                                <div class="footer-menu">
+                                    <h3>Service clients</h3>
+                                    <ul>
+                                        <li><a href="#">Contactez-nous</a></li>
+                                        <li><a href="#">Localisation Web</a></li>
+                                        <li><a href="#">Avis clients</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-lg-3 col-md-3 col-xs-12">
+                                <div class="footer-menu">
+                                    <h3>Information</h3>
+                                    <ul>
+                                        <li><a href="#">À propos</a></li>
+                                        <li><a href="#">Politique de confidentialité</a></li>
+                                        <li><a href="#">Termes et conditions</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-lg-3 col-md-3 col-xs-12">
+                                <div class="footer-menu">
+                                    <h3>Coordonnées</h3>
+                                    <ul>
+                                        <li><a href="#"><b>Adresse</b> : 2 rue des cèdres, 97354 Rémire-Montjoly</a></li>
+                                        <li><a href="#"><b>Téléphone</b> : 0594 30 46 67</a></li>
+                                        <li><a href="#"><b>Mail</b> : contact.alphamonetique@gmail.com</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- footer main end -->
+
+                <!-- footer copyright area -->
+                <div class="footer-copyright-area">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-12 col-lg-6 col-md-6">
+                                <p>Copyright © 2022-2023 AMI | Tous droits réservés | Développé par <a href="#">RyzenCode</p></a>
+                            </div>
+                            <div class="col-sm-12 col-lg-6 col-md-6 pull-right">
+                                <img src="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- footer copyright area end -->
+            </footer>
---
+++ index.php
@@ -0,0 +1,307 @@
+<php ?>
+    <!doctype html>
+    <html class="no-js" lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>AMI: Alpha Monétique Intéractif</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="images/favicon/favicon.ico" />
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/ionicons.min.css">
+        <link rel="stylesheet" href="css/css-plugins-call.css">
+        <link rel="stylesheet" href="css/bundle3.css">
+        <link rel="stylesheet" href="css/main6.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <link rel="stylesheet" href="css/colors.css">
+
+    </head>
+
+    <body>
+
+        <!-- Body main wrapper start -->
+        <div class="wrapper home-one">
+
+        <!-- Header Start -->
+        <?php include 'includes/header.php';?>
+        <!-- Header Done -->
+
+            <!-- Slider area -->
+            <div class="slider-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="slider-hidden col-lg-3">
+                            <!-- <h3></h3> -->
+                        </div>
+                        <div class="slider col-xl-9">
+                            <!-- Carousel-area start -->
+                            <div class="slider-area-inner">
+                                <!-- Carousel start -->
+                                <div class="slider-inner">
+                                    <div id="mainSlider" class="nivoSlider nevo-slider">
+                                        <img src="images/slider/slider.jpg" alt="main slider" title="#htmlcaption1" />
+                                        <img src="images/slider/slider-2.jpg" alt="main slider" title="#htmlcaption2" />
+                                    </div>
+                                    <div id="htmlcaption1" class="nivo-html-caption slider-caption">
+                                        <div class="slider-progress"></div>
+                                    </div>
+                                    <div id="htmlcaption2" class="nivo-html-caption slider-caption">
+                                        <div class="slider-progress"></div>
+                                    </div>
+                                </div>
+                                <!-- Carousel end -->
+                            </div>
+                            <!-- Carousel-area end -->
+                        </div>
+
+                        <!-- Banner Pax Start -->
+                        <div class="slider-banner-area col-sm-12">
+                            <div class="slider-banner">
+                                <div class="slider-single-banner">
+                                    <a href="shop.html">
+                                        <img src="images/banner/banner5.jpg" alt="Banner">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="slider-banner">
+                                <div class="slider-single-banner">
+                                    <a href="shop.html">
+                                        <img src="images/banner/banner6.jpg" alt="Banner">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Banner Pax Done -->
+                    </div>
+                </div>
+            </div>
+            <!-- Carousel area end -->
+            
+            <!-- Policy area -->
+            <div class="policy-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="policy-area-inner">
+                                <div class="single-policy-area">
+                                    <div class="single-policy">
+                                        <div class="icon"><i class="icon ion-android-sync"></i></div>
+                                        <h3>Mise à jour</h3>
+                                        <p>Rapide et instantanée!</p>
+                                    </div>
+                                </div>
+                                <div class="single-policy-area">
+                                    <div class="single-policy">
+                                        <div class="icon"><i class="icon ion-paper-airplane"></i></div>
+                                        <h3>Livraison</h3>
+                                        <p>Gratuite à partir de 500€</p>
+                                    </div>
+                                </div>
+                                <div class="single-policy-area">
+                                    <div class="single-policy">
+                                        <div class="icon"><i class="icon ion-help-buoy"></i></div>
+                                        <h3>Support 24/7</h3>
+                                        <p>Nos techniciens, à l'écoute </p>
+                                    </div>
+                                </div>
+                                <div class="single-policy-area">
+                                    <div class="single-policy">
+                                        <div class="icon"><i class="icon ion-card"></i></div>
+                                        <h3>Solution paiement</h3>
+                                        <p>Carte bancaire & espèces</p>
+                                    </div>
+                                </div>
+                                <div class="single-policy-area">
+                                    <div class="single-policy">
+                                        <div class="icon"><i class="icon ion-ios-locked"></i></div>
+                                        <h3>Paiement sécurisé</h3>
+                                        <p>Nous assurons votre sécurité</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Policy area end -->
+            <!-- Deals Of The Day -->
+            <div class="deals-of-the-day-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="section-title">
+                                <h3></h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="big-product-area">
+                        <div class="row">
+                            <div class="product-carousel-active owl-carousel">
+                                <div class="col-sm-12">
+                                    <!-- single product -->
+                                    <div class="single-product-area">
+                                        <div class="product-wrapper listview">
+                                            <div class="list-col4">
+                                                <div class="product-image">
+                                                    <a href="#">
+                                                        <img src="images/maintenance/bg2.png" alt="">
+                                                    </a>
+                                                    <div class="quickviewbtn">
+                                                        <a href="#" data-toggle="modal" data-target="#product_modal" data-original-title="Quick View"><i class="ion-eye"></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="list-col8">
+                                                <div class="product-info">
+                                                    <h2><a href="single-product.html">Pack Maintenance</a></h2>
+                                                    <span class="price">
+                                                        Pro
+                                                    </span>
+                                                    <div class="product-desc">
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco,Proin lectus
+                                                            ipsum, gravida et mattis vulputate, tristique ut lectus</p>
+                                                    </div>
+                                                    <div class="pack">
+                                                        <button>Découvrir</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- single product end -->
+                                </div>
+                                <div class="col-sm-12">
+                                    <!-- single product -->
+                                    <div class="single-product-area">
+                                        <div class="product-wrapper listview">
+                                            <div class="list-col4">
+                                                <div class="product-image">
+                                                    <a href="">
+                                                        <span class="onsale">Nouveau!</span>
+                                                        <img src="images/prog/logo3.png" alt="">
+                                                    </a>
+                                                    <div class="quickviewbtn">
+                                                        <a href="#" data-toggle="modal" data-target="#product_modal" data-original-title="Quick View"><i class="ion-eye"></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="list-col8">
+                                                <div class="product-info">
+                                                    <h2><a href="single-product.html">PROGRAMME</a></h2>
+                                                    <span class="price">
+                                                        <del></del>Logiciel de caisse
+                                                    </span>
+                                                    <div class="product-desc">
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco,Proin lectus
+                                                            ipsum, gravida et mattis vulputate, tristique ut lectus</p>
+                                                    </div>
+                                                    <div class="pack">
+                                                        <button>Découvrir</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- single product end -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Deals Of The Day -->
+            <!-- Home fullwidth banner -->
+            <div class="home-fullwidth-banner-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <a href="#">
+                                <img src="images/pub/pub2.png" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Home fullwidth banner end -->
+
+            <footer class="footer-area">
+                <!-- newsletter area -->
+                <div class="newsletter-area">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-12 col-xl-9">
+                                <div class="newsletter-text">
+                                    <div class="icon"><i class="icon ion-email"></i></div>
+                                    <h3>Abonnez-vous à notre newsletter</h3>
+                                    <h4>GRATUITEMENT</h4>
+                                </div>
+                                <div class="newsletter">
+                                    <div class="newsletter-box">
+                                        <form action="#">
+                                            <input class="subscribe" placeholder="votre adresse mail" name="email" id="subscribe" type="email">
+                                            <button type="submit" class="submit">Abonnez-vous!</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-xl-3">
+                                <div class="footer-social">
+                                    <ul class="social-icons">
+                                        <li><a class="facebook social-icon" href="#" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a class="instagram social-icon" href="#" title="Instagram" target="_blank"><i class="fa fa-instagram"></i></a></li>
+                                        <li><a class="rss social-icon" href="#" title="Rss" target="_blank"><svg style="margin-top:15px" xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                                            <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+                                          </svg></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        <!-- Footer Start -->
+        <?php include 'includes/footer.php'; ?>
+        <!-- Footer Done -->
+        </div>
+        <!-- Body main wrapper end -->
+
+
+        <!-- jQuery CDN -->
+        <script src="js/jquery-1.12.4.min.js"></script>
+
+        <!-- jQuery Local -->
+
+
+        <!-- Popper min js -->
+        <script src="js/popper.min.js"></script>
+        <!-- Bootstrap min js  -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- nivo slider pack js  -->
+        <script src="js/jquery.nivo.slider.pack.js"></script>
+        <!-- All plugins here -->
+        <script src="js/plugins.js"></script>
+        <!-- Main js  -->
+        <script src="js/main.js"></script>
+
+
+
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script>
+            window.ga = function() {
+                ga.q.push(arguments)
+            };
+            ga.q = [];
+            ga.l = +new Date;
+            ga('create', 'UA-XXXXX-Y', 'auto');
+            ga('send', 'pageview')
+        </script>
+        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+    </body>
+
+    </html>
