69a1937ac176cdbdca5ea6eae2f42f31a566fdfe
Bigends <chris.balkumar@outlook.fr>
2022-03-10 18:05:00 -0300

    edit et supp opé sauf img en cours

--- articles/TPE/admin/admin.php
+++ articles/TPE/admin/admin.php
@@ -42,6 +42,7 @@ require_once('includes/header.php');
 
             }
             ?><br>
+            <!-- Ajouter un article -->
             <h5>Titre</h5>
             <form method="POST" action="" enctype="multipart/form-data">
                 <input type="text" name="titre" placeholder="Titre de l'article" required="" class="form form-control"><h5>Prix</h5>
@@ -51,26 +52,63 @@ require_once('includes/header.php');
                 <input type="submit" name="submit" class="btn btn-light">
             </form>
 
+            <!-- Modifier et supprimer Début-->
             <?php
         }elseif ($_GET['action'] == 'edit_delete') {
             require_once("includes/database.php");
             $req_all_article = $db->prepare('SELECT *FROM tpe');
             $req_all_article->execute();
-
-            while ($reponse= $req_all_article->fetch(PDO::FETCH_OBJ)) {
+            ?> <table style="width: 100%"><?php
+            while ($reponse= $req_all_article->fetch(PDO::FETCH_OBJ)) { // Boutton Modif & Supp
                 ?>
-                <table style="width: 100%">
-                    <tr><td><?php echo $reponse->titre ?></td></tr>
-                    <tr><td><a href="">modifier</td></tr>
-                    <tr><td><a href="">supprimer</td></tr>
-                </table>
-                <?php 
+                    <tr><td><?php echo $reponse->titre ?></td>
+                    <td><a href="?action=edit&id=<?php echo $reponse->id ?>">modifier</a></td>
+                    <td><a href="?action=delete&id=<?php echo $reponse->id ?>">supprimer</a></td></tr>
+                <?php
             }
-        }
+            ?></table><?php 
+        }elseif ($_GET['action'] == 'delete') { // Supprimer l'article
+
+            require_once("includes/database.php");
+
+            $delete_article = $db->prepare('DELETE FROM tpe WHERE id=?');
+            $delete_article->execute(array($_GET['id']));
+            echo "<h5 style='color:white;border: solid 1px black;text-align:center;background-color:green;padding:3px;'> Article supprimé!</h5>";
+
+        }elseif ($_GET['action'] == "edit") { // Modifier l'article
+
+            require_once("includes/database.php");
+
+            if (isset($_POST['submit'])) { // Mis à jour
+
+                extract($_POST);
+
+                $article_update = $db->prepare('UPDATE tpe set titre=?,price=?,contenu=? WHERE id=?');
+                $article_update->execute(array($titre,$price,$contenu,$_GET['id']));
+                echo "<h5 style='color:white;border: solid 1px black;text-align:center;background-color:green;padding:3px;'> Article modifié!</h5>";
+            }
+
+            $req_all_value = $db->prepare('SELECT *FROM tpe WHERE id=?');
+            $req_all_value->execute(array($_GET['id']));
+            $reponses = $req_all_value->fetch(PDO::FETCH_OBJ);
+            ?>
+
+            <!-- Formulaire Modification et Suppression -->
+            <form method="POST" action="">
+                <h5>Titre</h5>
+                <input type="text" name="titre" value="<?php echo $reponses->titre ?>" required="" class="form form-control">
+                <h5>Prix</h5>
+                <input type="text" name="price" value="<?php echo $reponses->price ?>" required="" class="form form-control">
+                <h5>Description</h5>
+                <textarea name="contenu" require="" class="form form-control"><?php echo $reponses->contenu ?></textarea><br><br>
+                <input type="file" name="fichier"><br><br> <!-- Fichier Image En cours... -->
+                <input type="submit" name="submit" class="btn btn-light">
+            </form>
+            <?php
+        } // Modifier et supprimer Fin
     }
 
     ?>
-
 </div>
 
 <?php
